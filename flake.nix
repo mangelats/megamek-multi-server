7e86f73f77419bb196495a551955559cab47ae46
Martí Angelats i Ribera <marti.angelats@gmail.com>
2025-06-29 20:43:42 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "megamek.necronomicons.com";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+
+        deps = [
+          (pkgs.python3.withPackages (ps: [
+            ps.fastapi
+          ] ++ ps.fastapi.optional-dependencies.standard
+          ))
+        ];
+      in
+      {
+        packages = rec {
+        };
+        devShells.default = pkgs.mkShellNoCC {
+            packages = deps ++ [
+              pkgs.mypy
+            ];
+        };
+      }
+    );
+}
